857885a84ee9ddf54c3d68a73a07b7a135e30bfb
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-10-04 16:37:54 +0545

    initilaized project

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor (private configService: ConfigService){}
+  constructor(private configService: ConfigService) {}
   getHello(): string {
     return this.configService.get<string>('PORT');
   }
--- server/src/student/student.service.ts
+++ server/src/student/student.service.ts
@@ -1,3 +1,1 @@
-export class StudentService {
-    
-}
+export class StudentService {}
