220606f49fa909ac05178ff183f152806ba63f48
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-10-04 16:23:46 +0545

    dotenv and database setup done

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './DB/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor (private configService: ConfigService){}
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get<string>('PORT');
   }
 }
