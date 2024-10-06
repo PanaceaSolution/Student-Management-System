e86e719438b456972688f32399a197b7f9b73500
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-10-06 11:33:21 +0545

    refactor some changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './DB/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AppService } from './app.service';
       envFilePath: '.env',
     }),
     PrismaModule,
+    AuthenticationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
