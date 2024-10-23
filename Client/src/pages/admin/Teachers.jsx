40c513cc20d085f4c55ef20475d4a6aa1d2f1e12
Aayush Ghimire <aausgh@gmail.com>
2024-10-23 10:17:19 +0545

    search and filter removed

--- Client/src/pages/admin/Teachers.jsx
+++ Client/src/pages/admin/Teachers.jsx
@@ -44,25 +44,7 @@ const Teachers = () => {
   }, [getAllStaff]);
 
   // Memoized filtered staff data
-  const filteredStaff = useMemo(() => {
-    return staff.filter((item) => {
-      const matchesSearchTerm =
-        searchTerm === "" ||
-        item.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.userName.toLowerCase().includes(searchTerm.toLowerCase());
-
-      const matchesGender =
-        selectedGender === "" || item.gender === selectedGender;
-
-      const matchesRole = selectedRole === "" || item.role === selectedRole;
-
-      const matchesDate =
-        !date || new Date(item.dob).toDateString() === date.toDateString();
-
-      return matchesSearchTerm && matchesGender && matchesRole && matchesDate;
-    });
-  }, [staff, searchTerm, selectedGender, selectedRole, date]);
+  const filteredUser = staff.filter((staffMember) => staffMember.role === "Teacher")
 
   // Handle format selection and trigger export
   const handleExportChange = (event) => {
@@ -160,7 +142,7 @@ const Teachers = () => {
                     <span
                       className={`text-primary bg-gray-200 px-1 rounded ${activeTab === tab ? "" : ""}`}
                     >
-                      {filteredStaff.length}
+                      {filteredUser.length}
                     </span>
                   </a>
                 </div>
@@ -169,11 +151,11 @@ const Teachers = () => {
             <div className="relative w-full overflow-x-auto shadow-md">
               <StaffTable
                 title="Teacher"
-                user={filteredStaff}
+                user={filteredUser}
                 handleUserId={handleUserId}
               />
 
-              {filteredStaff?.length === 0 && (
+              {filteredUser?.length === 0 && (
                 <p className="text-center">Result Not Found</p>
               )}
             </div>
@@ -185,7 +167,7 @@ const Teachers = () => {
               <DetailsCard
                 title="Teacher"
                 selectedId={selectedId}
-                user={filteredStaff}
+                user={filteredUser}
               />
             </div>
           )}
