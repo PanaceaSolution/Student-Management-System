013dee8402f3f1e0d5d82b2b54b0a3cdd01c30a9
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-10-28 10:32:06 +0545

    few changes on middleware

--- server/src/auth.middleware.ts
+++ server/src/auth.middleware.ts
@@ -1,33 +1,33 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { JwtService } from '@nestjs/jwt';
+// import { Injectable, NestMiddleware } from '@nestjs/common';
+// import { Request, Response, NextFunction } from 'express';
+// import { JwtService } from '@nestjs/jwt';
 
-@Injectable()
-export class AuthMiddleware implements NestMiddleware {
-  constructor(private readonly jwtService: JwtService) {}
-  async use(req: Request, res: Response, next: NextFunction) {
-    try {
-      const token = req.cookies.accessToken; //you can either use authorization header tooo.
-      if (!token) {
-        return res.status(401).json({
-          message: 'Access Token Missing',
-          success: false,
-        });
-      }
-      const decoded = this.jwtService.verify(token);
-      if(decoded.role !== 'ADMIN'){
-        return res.status(403).json({
-          message:"Only Admin can Access",
-          success:false,
-        });
-      }
-      req.user = decoded;
-      next();
-    } catch (error) {
-      return res.status(401).json({
-        message: 'Invalid or expired access token',
-        success: false,
-      });
-    }
-  }
-}
+// @Injectable()
+// export class AuthMiddleware implements NestMiddleware {
+//   constructor(private readonly jwtService: JwtService) {}
+//   async use(req: Request, res: Response, next: NextFunction) {
+//     try {
+//       const token = req.cookies.accessToken; //you can either use authorization header tooo.
+//       if (!token) {
+//         return res.status(401).json({
+//           message: 'Access Token Missing',
+//           success: false,
+//         });
+//       }
+//       const decoded = this.jwtService.verify(token);
+//       if(decoded.role !== 'ADMIN'){
+//         return res.status(403).json({
+//           message:"Only Admin can Access",
+//           success:false,
+//         });
+//       }
+//       req.user = decoded;
+//       next();
+//     } catch (error) {
+//       return res.status(401).json({
+//         message: 'Invalid or expired access token',
+//         success: false,
+//       });
+//     }
+//   }
+// }
