68fadbe3c3a42196a613caf6b88b9adf77fcf2a5
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-10-29 16:34:16 +0545

    added RSA encryption and decryption functions

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,5 +1,6 @@
 import { ROLE } from './role.helper';
 import { STAFFROLE } from './role.helper';
+import * as crypto from 'crypto';
 
 export function generateRandomPassword(): string {
   return Math.random().toString(36).slice(-8);
@@ -26,7 +27,37 @@ export function generateUsername(
     } else if (staffRole === STAFFROLE.LIBRARIAN) {
       prefix = 'LB';
     }
+    prefix = 'STF'
   }
 
   return `${prefix}-${fname.charAt(0)}${lname.charAt(0)}${random_number}`;
+}
+
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+});
+
+export function encryptdPassword(password: string): string {
+  const buffer = Buffer.from(password, 'utf8');
+  const encrypted = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256',
+    },
+    buffer,
+  );
+  return encrypted.toString('base64');
+}
+export function decryptdPassword(encryptedPassword: string): string {
+  const buffer = Buffer.from(encryptedPassword, 'base64');
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256',
+    },
+    buffer,
+  );
+  return decrypted.toString('utf8');
 }
