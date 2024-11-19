57827159f20a6619c2a5e32d71ab93f7099311a3
Nik-doid <nikesh.shrestha2022@pcmgmt.edu.np>
2024-11-19 12:54:28 +0545

    a

--- server/src/student/entities/student.entity.ts
+++ server/src/student/entities/student.entity.ts
@@ -14,6 +14,7 @@ import { UUID } from 'typeorm/driver/mongodb/bson.typings';
 import { TRANSPORTATION_MODE } from '../../utils/role.helper';
 import { User } from '../../user/authentication/entities/authentication.entity';
 import { Assignment } from 'src/assignment/entities/assignment.entity';
+import { Course } from 'src/course/entities/course.entity';
 
 @Entity({ name: 'Student' })
 
@@ -63,8 +64,11 @@ export class Student {
   user: User;
   parent: any;
 
-  @OneToMany(() => Assignment, (assignment) => assignment.studentId)
+  @ManyToOne(() => Assignment, (assignment) => assignment.studentId)
   assignments: Assignment[];
+
+  @ManyToOne(() => Course, (course) => course.courseId)
+  course: Course[];
 
   // @ManyToOne(() => Parent, (parent) => parent.students, { nullable: true })
   // @JoinColumn({ name: 'parentId' })
