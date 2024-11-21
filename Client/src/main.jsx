e7dc59f37e75daa57b33011825fe89ec15a7062b
Aayush Ghimire <aausgh@gmail.com>
2024-11-21 16:08:40 +0545

    changed the position of logout to navbar

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { StrictMode } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { RefreshProvider } from './context/RefreshProvider.jsx'
 
