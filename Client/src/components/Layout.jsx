6ff6a98a58297d68b20d5ef1d8e6c70e558ccc10
Aayush Ghimire <aausgh@gmail.com>
2024-11-26 20:59:28 +0545

    loader for logging out

--- Client/src/components/Layout.jsx
+++ Client/src/components/Layout.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import useAuthStore from "@/store/authStore";
+import Spinner from "./Loader/Spinner";
 
 const Layout = () => {
+   const { isLoggingOut } = useAuthStore();
    return (
       <div className="grid h-screen w-full lg:grid-cols-[280px_1fr] bg-background transition-all duration-300">
          {/* Left Sidebar */}
@@ -20,6 +23,13 @@ const Layout = () => {
                <Outlet />
             </main>
          </div>
+
+         {isLoggingOut && (
+            <div className="absolute top-1/2 left-1/2 z-[100] transform -translate-x-1/2 -translate-y-1/2 bg-black/70 w-full h-full flex flex-col items-center justify-center">
+               <img className="w-20 h-20 animate-spin" src="https://www.svgrepo.com/show/491270/loading-spinner.svg" alt="Loading icon" />
+               <p className="text-center text-white text-4xl font-bold">Logging out...</p>
+            </div>
+         )}
       </div>
    );
 };
