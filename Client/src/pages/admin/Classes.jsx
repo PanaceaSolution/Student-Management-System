2cdae9e8a29dd15a578454d158c2b6ebbefaf5a8
Aayush Ghimire <aausgh@gmail.com>
2024-11-26 14:15:31 +0545

    added class form and store

--- Client/src/pages/admin/Classes.jsx
+++ Client/src/pages/admin/Classes.jsx
@@ -3,11 +3,14 @@ import SearchBox from '@/components/SearchBox';
 import Select from '@/components/Select';
 import { Button } from '@/components/ui/button';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import TableWithActions from '@/components/admin/tableWithActions';
+import useClassStore from '@/store/classStore';
+import AdminTable from '@/components/admin/AdminTable';
+import useExport from '@/hooks/useExport';
+import ClassForm from '@/components/admin/ClassForm';
 
 
-const classTableHead = ["Class Name", "Section", "Subject", "Class", "Action",];
-const classTableField = ["teacherName", "gender", "subject", "class"];
+const classTableHead = ["", "Class Name", "Section", "Action",];
+const classTableField = ["className", "section"];
 
 
 const Exports = [
@@ -20,23 +23,56 @@ const Exports = [
 
 const Class = () => {
   const [selectedExport, setSelectedExport] = useState("");
-  const [selectedGender, setSelectedGender] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
 
-  // Handle format selection and trigger export
-  const handleExportChange = (event) => {
-    const value = event.target.value;
-    setSelectedExport(value);
-    if (value === "CSV") {
-      exportToCSV();
-    } else if (value === "PDF") {
-      exportToPDF();
+  const { classes, getAllClasses, isDeleting } = useClassStore();
+
+  useEffect(() => {
+    const fetchClasses = async () => {
+      await getAllClasses();
     }
+
+    fetchClasses();
+  }, []);
+
+
+  const { exportToCSV, exportToPDF } = useExport();
+  // Handle format selection and trigger export
+  const handleExport = () => {
+    const exportHandlers = {
+      CSV: () => exportToCSV(subjects, "classes.csv"),
+      PDF: () => {
+        const headers = [
+          { header: "Class", dataKey: "className" },
+          { header: "Section", dataKey: "section" },
+          { header: "Class Teacher", dataKey: "classTeacher" },
+          { header: "Subjects", dataKey: "subject" },
+        ];
+        exportToPDF(subjects, headers, "Classes List", "classes.pdf");
+      }
+    };
+    if (selectedExport) exportHandlers[selectedExport]();
+  };
+
+  const handleExportChange = (event) => {
+    setSelectedExport(event.target.value);
+    handleExport();
   };
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  const handleEdit = (data) => {
+    setIsFormOpen(true);
+    setSelectedData(data);
+  }
+
+  const handleDelete = async (id) => {
+    await deleteClass(id);
+  }
 
   return (
     <section>
@@ -44,15 +80,19 @@ const Class = () => {
         <div className='rounded-sm bg-card lg:col-span-5 2xl:col-span-3 p-3'>
           <div className="flex justify-evenly sm:justify-end border-b-2 p-3">
             <div className="flex gap-3 md:gap-4">
-              <Button variant="print">
-                PRINT
-              </Button>
               <Select
                 options={Exports}
                 selectedValue={selectedExport}
                 onChange={handleExportChange}
                 className="w-32 bg-white"
               />
+              <Button
+                variant="create"
+                className="uppercase"
+                onClick={() => setIsFormOpen(true)}
+              >
+                Add Class
+              </Button>
             </div>
           </div>
           <div className="border-b-2 p-2">
@@ -66,26 +106,24 @@ const Class = () => {
               </div>
             </div>
           </div>
-
           <div className="relative w-full overflow-x-auto shadow-md">
-            <TableWithActions
+            <AdminTable
+              title="Classes"
               tableHead={classTableHead}
-              tableBody={classData}
-              tableFields={["teacherName", "gender", "subject", "class"]}
-              handleDelete={(data) => console.log(data)}
-              keyExtractor={(data) => data.id}
-              noDataMessage="No data available"
+              tableFields={classTableField}
+              user={classes}
+              loading={isDeleting}
+              handleUserData={(data) => console.log(data)}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
             />
-
-            {/* {filteredUser?.length === 0 && (
-                <p className="text-center">Result Not Found</p>
-              )} */}
           </div>
-
         </div>
-
       </div>
-
+      <ClassForm
+        isOpen={isFormOpen}
+        setIsOpen={setIsFormOpen}
+      />
     </section>
   );
 };
---
+++ Client/src/services/classServices.js
@@ -0,0 +1,41 @@
+const URL = import.meta.env.VITE_API_URL;
+
+export const getAllClassesService = async () => {
+   const response = await fetch(`${URL}/class`, {
+      method: "GET",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      credentials: "include",
+   });
+   return response.json();
+};
+
+export const createClassService = async (classData) => {
+   const response = await fetch(`${URL}/class/create`, {
+      method: "POST",
+      credentials: "include",
+      body: classData,
+   });
+   return response.json();
+};
+
+export const updateClassService = async (id, updatedClassData) => {
+   const response = await fetch(`${URL}/class/update/${id}`, {
+      method: "PATCH",
+      credentials: "include",
+      body: updatedClassData,
+   });
+   return response.json();
+};
+
+export const deleteClassService = async (id) => {
+   const response = await fetch(`${URL}/class/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      credentials: "include",
+   });
+   return response.json();
+};
