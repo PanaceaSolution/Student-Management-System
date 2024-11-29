7523503eb5a9f0171cd93bbd7391da1f45879c46
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-11-29 15:03:32 +0545

    removing console.log, saroj

--- server/src/notice/notice.service.ts
+++ server/src/notice/notice.service.ts
@@ -11,47 +11,53 @@ export class NoticeService {
   constructor(
     @InjectRepository(Notice)
     private readonly noticeRepository: Repository<Notice>,
-  ){}
+  ) {}
 
-  async create(createNoticeDto: CreateNoticeDto, file?: Express.Multer.File):Promise<Notice>{
+  async create(
+    createNoticeDto: CreateNoticeDto,
+    file?: Express.Multer.File,
+  ): Promise<Notice> {
     const notice = this.noticeRepository.create(createNoticeDto);
 
     if (file) {
-      const uploadResult = await uploadSingleFileToCloudinary(file,'notices');
-      console.log("Image", uploadResult);
-      
+      const uploadResult = await uploadSingleFileToCloudinary(file, 'notices');
+      // console.log('Image', uploadResult);
+
       notice.filePath = uploadResult.secure_url;
     }
     return this.noticeRepository.save(notice);
   }
 
-   async findAll(): Promise<Notice[]> {
-        return this.noticeRepository.find();
-    }
+  async findAll(): Promise<Notice[]> {
+    return this.noticeRepository.find();
+  }
 
-  async findOne(noticeID: string):Promise<Notice> {
-    const notice = await this.noticeRepository.findOne({where : {noticeID}});
-    if (!notice ){
+  async findOne(noticeID: string): Promise<Notice> {
+    const notice = await this.noticeRepository.findOne({ where: { noticeID } });
+    if (!notice) {
       throw new NotFoundException('Notice with ID ${noticeID} not found');
     }
     return notice;
   }
 
-  async update(noticeID : string, updateNoticeDto:UpdateNoticeDto,file?:Express.Multer.File): Promise<Notice> {
+  async update(
+    noticeID: string,
+    updateNoticeDto: UpdateNoticeDto,
+    file?: Express.Multer.File,
+  ): Promise<Notice> {
     const notice = await this.findOne(noticeID);
     Object.assign(notice, updateNoticeDto);
 
-    if(file){
-      const uploadResult = await uploadSingleFileToCloudinary(file,'notices');
-      notice.filePath = uploadResult.secure_url
+    if (file) {
+      const uploadResult = await uploadSingleFileToCloudinary(file, 'notices');
+      notice.filePath = uploadResult.secure_url;
     }
     return this.noticeRepository.save(notice);
-
   }
 
-  async remove(noticeID : string): Promise<{message : string}>{
+  async remove(noticeID: string): Promise<{ message: string }> {
     const result = await this.noticeRepository.delete(noticeID);
-    if (result.affected==0){
+    if (result.affected == 0) {
       throw new NotFoundException('Notice with ID ${noticeID} not found');
     }
     return { message: 'Deleted successfully' };
